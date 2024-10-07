1ffeeecf436e7fc6acebfe2a3f2476073aa9a5b7
avern2024 <avern2020@icloud.com>
2024-10-07 11:21:14 +0700

    Add files via upload

---
+++ module_1.9.py
@@ -0,0 +1,13 @@
+example = 'Python'
+print(example[0])
+print(example[-1])
+print(example[3:])
+print(example[::-1])
+print(example[1::2])
+print()
+example = 'Топинамбур'
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1::2])
