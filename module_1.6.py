dbd9bc5703969709affca92eb172ec28ecb50316
avern2024 <avern2020@icloud.com>
2024-10-07 10:04:54 +0700

    Add files via upload

---
+++ module_1.6.py
@@ -0,0 +1,5 @@
+Number_of_completed_homework_tasks = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_for_one_task = Number_of_hours_spent / Number_of_completed_homework_tasks
+print('Курс:', Course_name, ', всего задач:', Number_of_completed_homework_tasks, ', затрачено часов: ', Number_of_hours_spent, ', среднее время выполнения', Time_for_one_task, ' часа.')
