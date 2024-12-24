7f697af5a22f95e8370a9dc3a8fd354e6595ff29
avern2024 <avern2020@icloud.com>
2024-12-24 10:31:18 +0700

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,27 @@
+# st program
+print(9 ** 0.5 * 5)
+print()
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1001)
+print()
+
+# 3rd program
+first_expression = 2 * 2 + 2
+print(first_expression)
+second_expression = 2 * (2 + 2)
+print(second_expression)
+print(first_expression == second_expression)
+print()
+
+# 4th program
+float_ = float('123.456')
+float_mul = float_ * 10
+int_ = int(float_mul)
+result = int_ % 10
+print(result)
+print()
+
+# 4th program alternative solution
+string_ = '123.456'
+print(string_.split('.')[1][0])
